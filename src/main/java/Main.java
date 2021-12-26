65c978876693a837cb02b5b876c23ec48eac9a19
Iman-M02 <imanmohammadi@sharif.edu>
2021-12-26 06:14:40 +0330

    Main Class fixed

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import controller.AppController;
+import controller.AppRunner;
+import exception.BaseDirectoryInvalidException;
+import exception.SearchException;
+import model.Document;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AppRunner.startSearchEngine();
+    }
+}
